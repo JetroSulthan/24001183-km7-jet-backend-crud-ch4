6005c12dd85a1b6334064f74923382ed9b95b720
Brandon OddDuckk <Brandon.Ogaden@gmail.com>
2024-10-20 21:48:20 +0800

    feat: added getAllRentals function in rental controller

--- controllers/rentalController.js
+++ controllers/rentalController.js
@@ -1,12 +1,35 @@
 const { Rental } = require("../models");
+const { User, Car, Review } = require('../models');
 const imagekit = require("../lib/imagekit");
 
-const getAll = async (req, res) => {
-    res.render("rentals/index", { layout: 'layout' });
-    console.log("tes");
+const getAllRentals = async (req, res) => {
+    try {
+        const rentals = await Rental.findAll({
+            include: [
+                { model: User, attributes: ['name', 'email']},
+                { model: Car, attributes: ['model', 'no_plat']},
+                { model: Review, attributes: ['rating', 'komentar']},
+            ]
+        });
+
+        res.status(200).json({
+            status: "success",
+            message: "Rental data fetched successfully",
+            data: rentals,
+        });
+        console.log(rentals);
+
+        // TO DO : KEMBALIKAN DATA KE view index rentals
+        // res.render("rentals/index", { layout: 'layout' });
+    } catch (error) {
+        res.status(500).json({
+            status: "fail",
+            message: `fetching rental data failed. Error: ${error}`,
+        });
+    }
 }
 
 module.exports = {
-    getAll,
+    getAllRentals,
 };
 
