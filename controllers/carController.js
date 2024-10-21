085e7382b92ae1ad27bb8adc048119378f150891
NeoBitose <ramadhanalif430@gmail.com>
2024-10-21 11:27:50 +0700

    feat: add function for create cars

--- controllers/carController.js
+++ controllers/carController.js
@@ -1,1 +1,52 @@
-module.exports = {};
+const { Car } = require("../models");
+const imagekit = require("../lib/imagekit");
+
+async function createCar(req, res) {
+    try {
+        const file = req.file;
+        const split = file.originalname.split(".");
+        const ext = split[split.length - 1];
+        const {model, tahun, no_plat, status, harga} = req.body;    
+
+        const uploadedImage = await imagekit.upload({
+            file : file.buffer,
+            fileName: `${split[0]}-${Date.now()}.${ext}`
+        })
+
+        if (!uploadedImage) {
+            return res.status(400).json({
+                status: "Fail",
+                message: "Fail to upload image",
+                isSuccess: false,
+                data: null,
+            });
+        }
+
+        const newCar = await Car.create({
+            model, 
+            tahun, 
+            no_plat, 
+            status, 
+            harga, 
+            foto_mobil: uploadedImage.url
+        });
+        res.status(200).json({
+            status: "Success",
+            message: "Successfully added car data",
+            isSuccess: true,
+            data: { newCar, foto_mobil: uploadedImage.url },
+        });
+    } 
+    catch (error) {
+        res.status(500).json({
+            status: "Fail",
+            message: error.message,
+            isSuccess: false,
+            data: null,
+        });
+    }
+}
+
+module.exports = {
+    createCar,
+};
--- routes/carsRoute.js
+++ routes/carsRoute.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const carController = require("../controllers/carController");
+const upload = require('../middleware/uploader');
 
 //Route disini
+router.post("/", upload.single("foto_mobil"), carController.createCar);
 
 module.exports = router;
