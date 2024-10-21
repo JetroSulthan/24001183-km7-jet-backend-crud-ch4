ac8247dc1705423efc4a1206cba64a86a6ddfffa
RifqiAfandi <rifqitriafandi.20@gmail.com>
2024-10-21 17:53:12 +0700

    feat: make update review

--- routes/reviewsRoute.js
+++ routes/reviewsRoute.js
@@ -6,5 +6,7 @@ const reviewController = require("../controllers/reviewController");
 router.get('/', reviewController.getReviews)
 router.get("/create-review", reviewController.createPage);
 router.post("/create-review", reviewController.createReview);
+router.get("/update-review/:id", reviewController.updatePage);
+router.post("/update-review/:id", reviewController.updateReview);
 
 module.exports = router;
