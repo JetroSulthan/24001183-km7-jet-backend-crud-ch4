e0f63ee0274044de8c6a188a21ea217cad5ea798
RifqiAfandi <rifqitriafandi.20@gmail.com>
2024-10-21 07:01:02 +0700

    feat: Create review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,1 +1,38 @@
-module.exports = {};
+const { Review } = require("../models");
+const { Rental } = require("../models");
+
+async function createReview(req, res) {
+    const { rental_id, rating, komentar, tgl_review } = req.body;
+    try {
+        const newReview = {
+            rental_id,
+            rating,
+            komentar,
+            tgl_review: tgl_review || new Date()
+        };
+        await Review.create(newReview)
+        res.redirect("create-review");
+    } catch (error) {
+        res.status(500).json({
+            status: "Failed",
+            message: error.message,
+            isSuccess: false
+        });
+    }
+};
+async function createPage(req, res) {
+    try {
+        const rentals = await Rental.findAll();
+        res.render("reviews/create-review", { layout: 'layout', rentals});
+    } catch (error) {
+        res.status(500).json({
+            status: "Failed",
+            message: error.message,
+            isSuccess: false
+        });
+    }
+}
+module.exports = {
+    createReview,
+    createPage
+};
--- routes/reviewsRoute.js
+++ routes/reviewsRoute.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const reviewController = require("../controllers/reviewController");
 
 //Route disini
+router.get("/create-review", reviewController.createPage);
+router.post("/create-review", reviewController.createReview);
 
 module.exports = router;
