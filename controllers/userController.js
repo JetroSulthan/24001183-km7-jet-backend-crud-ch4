3e41e553f1391999d97b02764df836be9a1e74e6
Rafly Aziz Abdillah <raflyazizabdillah30@gmail.com>
2024-10-23 02:20:41 +0700

    fix: fix bug in users details

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ const imagekit = require("../lib/imagekit");
 async function readAllUsers(req, res) {
   try {
     const users = await User.findAll();
-    res.render("admin/userList", { users });
+    res.render("users/index", { users });
   } catch (error) {
     console.error(error);
     res.status(500).render("errors/500", {
